e31e0332874fd63e2c8e81bb260c380cdaea692d
eoin r <C13383656@mydit.ie>
2015-09-30 14:12:54 +0000

    added Palandrome

---
+++ Lab3/Palandrome.py
@@ -0,0 +1,12 @@
+import math
+testWord = raw_input("enter a word to check:")
+testWord.replace(" ","")
+length = len(testWord)
+halfLength = int(length/2)
+
+for i in range(0, halfLength):
+  if testWord[i] != testWord[(length - 1) - i]:
+    print "not a palandrome"
+    break
+  if i == (halfLength -1):
+     print "is a palandrome"
