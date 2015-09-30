d16f8a81204a91fcda1c8feca88890b262a10f88
eoin r <C13383656@mydit.ie>
2015-09-30 14:32:24 +0000

    Final working tested version

--- Lab3/Palandrome.py
+++ Lab3/Palandrome.py
@@ -1,5 +1,5 @@
-
 testWord = raw_input("enter a word to check:")
+testWord.replace(" ","")
 length = len(testWord)
 halfLength = int(length/2)
 #for loop checks the first character against the last,
@@ -8,5 +8,6 @@ halfLength = int(length/2)
 for i in range(0, halfLength):
   if testWord[i] != testWord[(length - 1) - i]:
     print "not a palandrome"
+    break
   if i == (halfLength -1):
      print "is a palandrome"
