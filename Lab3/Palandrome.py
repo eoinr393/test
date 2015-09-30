abf5beb5cc62e6b1fbf5f438bfc0014f8bb400c5
eoin r <C13383656@mydit.ie>
2015-09-30 14:29:03 +0000

    First commit

--- Lab3/Palandrome.py
+++ Lab3/Palandrome.py
@@ -1,13 +1,1 @@
-testWord = raw_input("enter a word to check:")
-testWord.replace(" ","")
-length = len(testWord)
-halfLength = int(length/2)
-#for loop checks the first character against the last,
-#then moves in a step and checks the next two sets,
-#because half of the length is an int there are no problems with odd numbers
-for i in range(0, halfLength):
-  if testWord[i] != testWord[(length - 1) - i]:
-    print "not a palandrome"
-    break
-  if i == (halfLength -1):
-     print "is a palandrome"
+
