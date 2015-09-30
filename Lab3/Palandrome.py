a02af8964b10e6fa015bf09a40539429eda86953
eoin r <C13383656@mydit.ie>
2015-09-30 14:30:36 +0000

    Second commit

--- Lab3/Palandrome.py
+++ Lab3/Palandrome.py
@@ -1,1 +1,12 @@
 
+testWord = raw_input("enter a word to check:")
+length = len(testWord)
+halfLength = length/2
+#for loop checks the first character against the last,
+#then moves in a step and checks the next two sets,
+#because half of the length is an int there are no problems with odd numbers
+for i in range(0, halfLength):
+  if testWord[i] != testWord[(length - 1) - i]:
+    print "not a palandrome"
+  if i == (halfLength -1):
+     print "is a palandrome"
