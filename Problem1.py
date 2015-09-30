66ef3d0248f264f8550d820280c20ae2ff3f47ec
eoin r <C13383656@mydit.ie>
2015-09-30 13:00:29 +0000

    first commit

---
+++ Problem1.py
@@ -0,0 +1,17 @@
+sum1 = 0
+sum2 = 0
+num1 = 3
+num2 = 5
+
+while (num1 < 1000):
+  sum1 += num1
+  num1 += num1
+
+while (num2 < 1000):
+  sum2 += num2
+  num2 += num2
+
+print "1:", sum1
+print "2:", sum2
+
+
